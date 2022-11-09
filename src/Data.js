63a77283043fbb35dc66226231b3e00ca9b0e316
ShivaSenaReddy <shivasenajagapathi7@gmail.com>
2022-11-09 13:07:11 +0530

    changes

--- src/Data.js
+++ src/Data.js
@@ -1,11 +1,13 @@
 const Data = [
     {
         Date: '09-11-2022',
-        Description: `I couldnt be more productive today as I was distracted a lot.
+        Description: `
+I couldn't be more productive today as I was distracted a lot.
         I need to control my thoughts and live more in the present.
-Control bad thoughts. Do something everyday, disctract with a new good habit, no peeking.
-I am goining take example of Marcus Aurileus the greatest emporer who is going to sty in control no matter the situation
-and thoughts.        
+Control bad thoughts. Do something every day, distract yourself with a new good habit, no peeking.
+I am going to take the example of Marcus Aurelius the greatest Emporer who is going to stay in control no matter the situation
+and thoughts. I am going to take responsibility for my life and I am going to be more in control of my life    
+
 `
     },
     {
