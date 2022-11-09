28273fa84c28c54d6ad94d085a5609d891f36671
ShivaSenaReddy <shivasenajagapathi7@gmail.com>
2022-11-09 16:30:45 +0530

    changes

--- src/Day.js
+++ src/Day.js
@@ -1,7 +1,7 @@
 export default function Day(props) {
     return (<div className='Day'>
         <h3>{props.Date}</h3>
-        <p>
+        <p className='description'>
             {props.Description} 
         </p>
         </div>
