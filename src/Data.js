d3722ca9c1835ff0b0864f68ce991510421eea7f
ShivaSenaReddy <shivasenajagapathi7@gmail.com>
2022-11-09 13:50:46 +0530

    changes

--- src/Data.js
+++ src/Data.js
@@ -12,11 +12,21 @@ I am going to be more sober and thus increase my AURA and stay in the best possi
 Groom nicely.
 `
     },
+
     {
         Date: '08-11-2022',
         Description: `
-I am extraordinary
-
+I am extraordinary.
+I need to work more on my self control. I need to be more productive.
+Somedays I feel bad and nervous. Hence I need to increase my confidence and you know what I should do for that.
+So just align your habits perfectly for that.
+I am going to have a proper schedule. No phone upto 1hr of waking up and then
+immediate workout.
+If possible try to wake up before 7 am on days you dont go to office.
+You are perfectly healthy so just be fine with your thoughts.
+Stay conscious of your breathing and enjoy everything.
+Work on your websit 30 mins per day. Daily learning 1.5 hrs.
+Office work try to wrap up in 4 hrs everyday with Deep Work(its not possible but try)
 `
     },
 
